9e744833a5c871ef971d7551bc42e1e9b69da5e8
samgos <samuel.jj.gosling@gmail.com>
2020-01-29 00:35:16 +0000

    Updates

---
+++ precache-manifest.bce9f58e3d37119329d446e81034fbf6.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "f0b489a5dbbff08833d21024f9fcbd4e",
+    "url": "/burnSignal/static/media/nucleo.f0b489a5.woff"
+  },
+  {
+    "revision": "b17a118e13e53558658b681a0ebdad82",
+    "url": "/burnSignal/static/media/nucleo.b17a118e.ttf"
+  },
+  {
+    "revision": "5987dd12fea78ce5f97ae601b08ec03c",
+    "url": "/burnSignal/static/media/nucleo.5987dd12.woff2"
+  },
+  {
+    "revision": "03ef1918e505c3e3471f9369ef7a638f",
+    "url": "/burnSignal/static/media/nucleo.03ef1918.eot"
+  },
+  {
+    "revision": "0a58105d6fb2c9c949a011a4d3f05379",
+    "url": "/burnSignal/static/media/logo.0a58105d.png"
+  },
+  {
+    "revision": "b72026857978535d8d7c",
+    "url": "/burnSignal/static/js/runtime~main.b7202685.js"
+  },
+  {
+    "revision": "18fcff59508740800f8a",
+    "url": "/burnSignal/static/js/main.18fcff59.chunk.js"
+  },
+  {
+    "revision": "a8b0929d5bfbc5da26ce",
+    "url": "/burnSignal/static/js/1.a8b0929d.chunk.js"
+  },
+  {
+    "revision": "18fcff59508740800f8a",
+    "url": "/burnSignal/static/css/main.e09005e5.chunk.css"
+  },
+  {
+    "revision": "1017f27f0290b8c13f1e47ec3493cff0",
+    "url": "/burnSignal/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/burner-signal/precache-manifest.c441fd205c353ad6720f01184535c875.js"
+  "/burnSignal/precache-manifest.bce9f58e3d37119329d446e81034fbf6.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/burner-signal/index.html", {
+workbox.routing.registerNavigationRoute("/burnSignal/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
