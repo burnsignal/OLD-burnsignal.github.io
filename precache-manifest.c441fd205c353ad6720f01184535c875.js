74224fc55216dc3757c25bea3d70b446593c2a0d
samgos <samuel.jj.gosling@gmail.com>
2020-01-29 00:30:49 +0000

    Updates

---
+++ precache-manifest.c441fd205c353ad6720f01184535c875.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "f0b489a5dbbff08833d21024f9fcbd4e",
+    "url": "/burner-signal/static/media/nucleo.f0b489a5.woff"
+  },
+  {
+    "revision": "b17a118e13e53558658b681a0ebdad82",
+    "url": "/burner-signal/static/media/nucleo.b17a118e.ttf"
+  },
+  {
+    "revision": "5987dd12fea78ce5f97ae601b08ec03c",
+    "url": "/burner-signal/static/media/nucleo.5987dd12.woff2"
+  },
+  {
+    "revision": "03ef1918e505c3e3471f9369ef7a638f",
+    "url": "/burner-signal/static/media/nucleo.03ef1918.eot"
+  },
+  {
+    "revision": "0a58105d6fb2c9c949a011a4d3f05379",
+    "url": "/burner-signal/static/media/logo.0a58105d.png"
+  },
+  {
+    "revision": "17ce1b5fdb78482e9f25",
+    "url": "/burner-signal/static/js/runtime~main.17ce1b5f.js"
+  },
+  {
+    "revision": "b9aea6db721dd520728f",
+    "url": "/burner-signal/static/js/main.b9aea6db.chunk.js"
+  },
+  {
+    "revision": "38b1e12c2bbed7c47fee",
+    "url": "/burner-signal/static/js/1.38b1e12c.chunk.js"
+  },
+  {
+    "revision": "b9aea6db721dd520728f",
+    "url": "/burner-signal/static/css/main.3ac36f51.chunk.css"
+  },
+  {
+    "revision": "a6cb8dbf54b925b763baf16a1fcc9c93",
+    "url": "/burner-signal/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a62bb613d112245cc537bb1f58bcbe3f.js"
+  "/burner-signal/precache-manifest.c441fd205c353ad6720f01184535c875.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/burner-signal/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
