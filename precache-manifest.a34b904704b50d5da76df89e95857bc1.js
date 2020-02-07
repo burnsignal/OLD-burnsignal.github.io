9a2e4a199d11a6737f86beecff05e9136d943a35
samgos <samuel.jj.gosling@gmail.com>
2020-02-07 04:42:33 +0000

    Updates

---
+++ precache-manifest.a34b904704b50d5da76df89e95857bc1.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "f0b489a5dbbff08833d21024f9fcbd4e",
+    "url": "/burnSignal/static/media/nucleo.f0b489a5.woff"
+  },
+  {
+    "revision": "b17a118e13e53558658b681a0ebdad82",
+    "url": "/burnSignal/static/media/nucleo.b17a118e.ttf"
+  },
+  {
+    "revision": "5987dd12fea78ce5f97ae601b08ec03c",
+    "url": "/burnSignal/static/media/nucleo.5987dd12.woff2"
+  },
+  {
+    "revision": "03ef1918e505c3e3471f9369ef7a638f",
+    "url": "/burnSignal/static/media/nucleo.03ef1918.eot"
+  },
+  {
+    "revision": "0a58105d6fb2c9c949a011a4d3f05379",
+    "url": "/burnSignal/static/media/logo.0a58105d.png"
+  },
+  {
+    "revision": "b72026857978535d8d7c",
+    "url": "/burnSignal/static/js/runtime~main.b7202685.js"
+  },
+  {
+    "revision": "72871255bb262c581ee0",
+    "url": "/burnSignal/static/js/main.72871255.chunk.js"
+  },
+  {
+    "revision": "cb9fe12770e41899b01f",
+    "url": "/burnSignal/static/js/1.cb9fe127.chunk.js"
+  },
+  {
+    "revision": "72871255bb262c581ee0",
+    "url": "/burnSignal/static/css/main.72fcc5ca.chunk.css"
+  },
+  {
+    "revision": "b7285826dd4c4d3f15428ae9ec0a5fbe",
+    "url": "/burnSignal/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/burnsignal/precache-manifest.2f74b1da4f36dcb10dd7973eea792811.js"
+  "/burnSignal/precache-manifest.a34b904704b50d5da76df89e95857bc1.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/burnsignal/index.html", {
+workbox.routing.registerNavigationRoute("/burnSignal/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
