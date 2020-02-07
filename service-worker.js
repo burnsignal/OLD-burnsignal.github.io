1ccecdeb7639750bb44982d71460bf36f6af767a
samgos <samuel.jj.gosling@gmail.com>
2020-02-07 04:44:51 +0000

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/burnSignal/precache-manifest.a34b904704b50d5da76df89e95857bc1.js"
+  "/burnsignal/precache-manifest.77acfa8852762a4cef81df1555adea2e.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/burnSignal/index.html", {
+workbox.routing.registerNavigationRoute("/burnsignal/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
